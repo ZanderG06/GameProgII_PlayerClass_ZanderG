bfc9db92af1f68612c37066de1d66331363e6f4b
Zander Goodwin <zanderngoodwin@gmail.com>
2026-01-07 20:01:42 -0400

    100 Players are displayed

--- GameProgII_PlayerClass_ZanderG/Program.cs
+++ GameProgII_PlayerClass_ZanderG/Program.cs
@@ -10,21 +10,76 @@ namespace GameProgII_PlayerClass_ZanderG
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player();
-            Player player2 = new Player();
+            //Player player1 = new Player(100, 1);
+            //Player player2 = new Player();
+            List<Player> list = new List<Player>(100);
+            Random random = new Random();
 
+            
+            /*
             player1.SetName("Zander");
             player2.SetName("Lucas");
 
-            player1.
+            player1.PrintStats();
+            player2.PrintStats();
+            Console.ReadKey(true);
+            Console.Clear();
+
+            Console.WriteLine($"{player2.GetName()} took damage!");
+            Console.WriteLine($"{player1.GetName()} leveled up!");
+            player2.TakeDamage(30);
+            player1.LevelUp();
+            Console.ReadKey(true);
+            Console.Clear();
+
+            player1.PrintStats();
+            player2.PrintStats();
+            Console.ReadKey(true);
+            Console.Clear();
+
+            Console.WriteLine("Hitting enter will generate 100 players and display their stats");
+            Console.ReadKey(true);
+            Console.Clear();
+            */
+
+            for (int i = 0; i < 100; i++)
+            {
+                
+                int health = random.Next(100);
+                int level = random.Next(100);
+                list.Add(new Player(health, level));
+                list[i].SetName($"Player{i + 1}");
+                list[i].PrintStats();
+            }
+
+            /*
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i].SetName($"Player{i}");
+
+                
+                
+
+                //list[i].TakeDamage(damage);
+
+                int level = random.Next(100);
+
+                for (int j = 0; j < level; j++)
+                {
+                    list[i].LevelUp();
+                }
+
+                list[i].PrintStats();
+            }
+            */
         }
     }
 
     class Player
     {
         string _name;
-        int _health;
-        int _level;
+        int _health = 100;
+        int _level = 1;
 
         public void TakeDamage(int damage)
         {
@@ -48,7 +103,18 @@ namespace GameProgII_PlayerClass_ZanderG
 
         public void PrintStats()
         {
-            Console.WriteLine($"Name{_name} Health: {_health} Level: {_level}");
+            Console.WriteLine($"Name: {_name} Health: {_health} Level: {_level}");
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public Player(int health, int level)
+        {
+            _health = health;
+            _level = level;
         }
     }
 }
